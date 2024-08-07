55ec8b94e67c2005158477a5723eb2eafdd289bb
samajestecodeur <issa97403@gmail.com>
2024-08-07 15:59:30 +0200

    server mythot first

---
+++ data/data.js
@@ -0,0 +1,68 @@
+
+export const pays_data = [
+    {id: "01809c46-5804-457f-908e-9a0f57b711a7" ,titre : "Côte d'ivoire"}
+]
+
+
+export const regions_data = [
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lagunes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lacs" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Haut-Sassandra" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Montagnes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Savanes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Bas-Sassandra" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Savanes" },
+  {
+    pays_id: "01809c46-5804-457f-908e-9a0f57b711a7",
+    titre: "Vallée du Bandama",
+  },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lacs" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lagunes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Worodougou" },
+  {
+    pays_id: "01809c46-5804-457f-908e-9a0f57b711a7",
+    titre: "Sassandra-Marahoué",
+  },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Zanzan" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Savanes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Montagnes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lagunes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Bas-Sassandra" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lagunes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Denguélé" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Woroba" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Comoé" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lagunes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Comoé" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Abidjan" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lacs" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Gôh-Djiboua" },
+  {
+    pays_id: "01809c46-5804-457f-908e-9a0f57b711a7",
+    titre: "Sassandra-Marahoué",
+  },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Bas-Sassandra" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Montagnes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Woroba" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lagunes" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Zanzan" },
+  {
+    pays_id: "01809c46-5804-457f-908e-9a0f57b711a7",
+    titre: "Vallée du Bandama",
+  },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Fromager" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "N’zi-Comoé" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Zanzan" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Worodougou" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Fromager" },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Bas-Sassandra" },
+  {
+    pays_id: "01809c46-5804-457f-908e-9a0f57b711a7",
+    titre: "Dix-Huit Montagnes",
+  },
+  {
+    pays_id: "01809c46-5804-457f-908e-9a0f57b711a7",
+    titre: "Dix-Huit Montagnes",
+  },
+  { pays_id: "01809c46-5804-457f-908e-9a0f57b711a7", titre: "Lacs" },
+];
